9986bf692fd4477773af6a27c29a3a3580fac31a
frontendmonster <frontendmonster@gmail.com>
2018-09-13 21:01:34 +0430

    Use seedDB

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,5 +1,5 @@
 import { ApolloServer } from 'apollo-server-express';
-import { connectDB, createRedisInstance } from '@merns/dal';
+import { RedisClient, MongooseClient, seedDB } from '@merns/dal';
 import { typeDefs, resolvers, context } from './graphql';
 import { app } from './app';
 import { logger } from './api-logger';
@@ -10,8 +10,9 @@ const logServerStat = () => {
 
 export const startServer = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    await createRedisInstance(process.env.REDIS_URI);
+    await MongooseClient.createClient(process.env.MONGO_URI);
+    await RedisClient.createClient(process.env.REDIS_URI);
+    await seedDB();
   } catch (e) {
     logger.error(e.message);
     return;
