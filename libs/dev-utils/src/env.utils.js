ab7edd05826717ad47447dc6d883d10ee49a8738
frontendmonster <frontendmonster@gmail.com>
2018-09-16 01:23:54 +0430

    Convert spread operator to Object.assign

--- libs/dev-utils/src/env.utils.js
+++ libs/dev-utils/src/env.utils.js
@@ -10,13 +10,12 @@ const get = () => process.env.NODE_ENV;
 
 const set = _ => crossEnv(`NODE_ENV=${_}`);
 
-const envUtils = {
-  ...ENVS,
+const envUtils = Object.assign({}, ENVS, {
   get,
   set,
   isDev: ENVS.dev.includes(get()),
   isProd: ENVS.prod.includes(get()),
   isTest: ENVS.test.includes(get()),
-};
+});
 
 module.exports = envUtils;
