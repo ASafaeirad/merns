3897b114fdd0d575c81f501ac793c5e3d2212253
frontendmonster <frontendmonster@gmail.com>
2018-09-15 19:54:01 +0430

    Add common package

---
+++ packages/common/src/constants/tokens.js
@@ -0,0 +1,6 @@
+export const AUTH_TOKEN = 'merns_token';
+export const REFRESH_TOKEN = 'merns_refresh_token';
+export const USER_ID_TOKEN = 'merns_id';
+
+export const GRAPHQL_HOST = 'http://localhost:4000';
+export const GRAPHQL_URL = `${GRAPHQL_HOST}/graphql`;
