d6a2acb7b125b8dfdf6c680b58f80a1feb1bd5a7
frontendmonster <frontendmonster@gmail.com>
2018-09-18 01:19:31 +0430

    Add main gulpfile to build and dev packages

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const fs = require('fs');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const through = require('through2');
+const newer = require('gulp-newer');
+const babel = require('gulp-babel');
+const gulpWatch = require('gulp-watch');
+const merge = require('merge-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const nodemon = require('gulp-nodemon');
+const { Signale, chProcessing } = require('@fem/logger');
+
+const gulpLogger = Signale('gulp');
+
+const sources = [
+  'packages/api',
+  'packages/common',
+  'packages/dal',
+];
+
+// WARNING: This method is not flexible and just work for this file structures
+const getBabelConfigs = packages => packages.map((source) => {
+  const config = JSON.parse(fs.readFileSync(path.join(source, '.babelrc'), 'utf8'));
+  let extendsConfig = {};
+
+  if (config.extends) {
+    extendsConfig = JSON.parse(fs.readFileSync(path.join(source, config.extends), 'utf8'));
+  }
+
+  delete config.extends;
+  return Object.assign({}, extendsConfig, config);
+});
+
+const babelConfigs = getBabelConfigs(sources);
+
+function swapSrcWithDist(srcPath) {
+  const parts = srcPath.split(path.sep);
+  const srcIndex = parts.findIndex(part => part === 'src');
+
+  parts[srcIndex] = 'dist';
+  return parts.join(path.sep);
+}
+
+function getGlobFromSource(source) {
+  return `./${source}/src/**/*.js`;
+}
+
+const compilationLogger = () => through.obj((file, _, callback) => {
+  gulpLogger.success(`Compiling '${chProcessing(file.relative)}'...`);
+  callback(null, file);
+});
+
+const logErrors = () => plumber({
+  errorHandler(err) {
+    gulpLogger.error(err.stack);
+  },
+});
+
+function rename(fn) {
+  return through.obj((file, enc, callback) => {
+    file.path = fn(file);
+    callback(null, file);
+  });
+}
+
+function build() {
+  return merge(
+    sources.map((source, i) => {
+      const base = path.join(__dirname, source);
+
+      return gulp
+        .src(getGlobFromSource(source), { base })
+        .pipe(logErrors())
+        .pipe(newer({ dest: base, map: swapSrcWithDist }))
+        .pipe(compilationLogger())
+        .pipe(sourcemaps.init())
+        .pipe(babel(babelConfigs[i]))
+        .pipe(sourcemaps.write('.'))
+        // Passing 'file.relative' because newer() above uses a relative path and this keeps it consistent.
+        .pipe(rename(file => path.resolve(file.base, swapSrcWithDist(file.relative))))
+        .pipe(gulp.dest(base));
+    }),
+  );
+}
+
+const watchBuild = () => gulpWatch(sources, { debounceDelay: 200 }, gulp.task('build'));
+
+const startServer = () => {
+  nodemon({ script: './packages/api/dist', watch: [getGlobFromSource(sources)], quiet: true, verbose: false });
+};
+
+gulp.task('start', startServer);
+
+gulp.task('build', build);
+
+gulp.task('watch', gulp.series('build', watchBuild));
+
+gulp.task('default', gulp.series('build'));
+
+gulp.task('dev', gulp.series('build', gulp.parallel('start', watchBuild)));
--- package-scripts.js
+++ package-scripts.js
@@ -1,51 +1,34 @@
-const { series, concurrent } = require('nps-utils');
+const { series } = require('nps-utils');
+const { env } = require('@fem/dev-utils');
 
 module.exports.scripts = {
-  clean: 'lerna run clean',
+  clean: {
+    default: 'lerna run clean',
+    core: 'lerna run clean --scope @merns/*',
+    libs: 'lerna run clean --scope @fem/*',
+  },
 
   lint: {
     default: 'lerna run lint',
     core: 'lerna run lint --scope @merns/*',
-    libs: 'lerna run lint --scope @fem/*'
+    libs: 'lerna run lint --scope @fem/*',
   },
 
   build: {
     default: 'lerna run build',
     core: 'lerna run build --scope @merns/*',
-    libs: 'lerna run build --scope @fem/*'
+    libs: 'lerna run build --scope @fem/*',
   },
 
   watch: {
     default: 'lerna run build:watch --parallel',
     core: 'lerna run build:watch --parallel --scope @merns/*',
-    libs: 'lerna run build:watch --parallel --scope @fem/*'
+    libs: 'lerna run build:watch --parallel --scope @fem/*',
   },
 
-  dev: {
-    default: concurrent({
-      api: {
-        script: 'yarn workspace @merns/api run dev',
-        color: 'null.bold'
-      },
-      dal: {
-        script: 'yarn workspace @merns/dal run build:watch',
-        color: 'null.bold'
-      },
-      controller: {
-        script: 'yarn workspace @merns/controller run build:watch',
-        color: 'null.bold'
-      },
-      common: {
-        script: 'yarn workspace @merns/common run build:watch',
-        color: 'null.bold'
-      },
-      web: {
-        script: 'yarn workspace @merns/web run dev',
-        color: 'null.bold'
-      },
-    }),
-  },
-  test: `lerna run test`,
+  dev: `${env.set(env.dev)} gulp dev`,
 
-  check: series('nps clean', 'nps build', 'nps test', 'yarn lint')
+  test: 'lerna run test',
+
+  check: series('nps clean', 'nps build', 'nps test', 'yarn lint'),
 };
